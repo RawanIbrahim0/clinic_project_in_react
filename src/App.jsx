cb3ee07083eef3a96fafd89b53496db3500733b1
RawanIbrahim0 <hlwshhyfa@gmail.com>
2025-10-28 19:56:41 +0300

    initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-amber-50 text-black text-6xl'>I'm Rawan Ibrahim </h1>
+           </>
+  )
+}
+
+export default App
