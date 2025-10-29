2ac9fed37ff4b0f98885482a543cd0beb60ad5c4
RawanIbrahim0 <hlwshhyfa@gmail.com>
2025-10-29 22:16:06 +0300

    anyy edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ function App() {
 
   return (
     <>
-       <NavBar/>
- 
-    </>
+{/*        <NavBar/>
+ */} 
+ <div className='flex'>
+ <div className='bg-[var(--black)] w-50 h-50'></div>
+ <div className='bg-[var(--dark-green)] w-50 h-50'></div>
+ <div className='bg-[var(--caribbean-current)] w-50 h-50'></div>
+ <div className='bg-[var(--persian-green)] w-50 h-50'></div>
+ <div className='bg-[var(--robin-egg-blue)] w-50 h-50'></div>
+ <div className='bg-[var(--timberwolf)] w-50 h-50'></div>
+ </div>
+  </>
   )
 }
 
